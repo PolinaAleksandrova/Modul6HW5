8a738282cfe69b8a423454eaa6960de0206397b0
Polina Aleksandrova <polina.aleksandrova@nure.ua>
2022-01-30 21:00:07 +0200

    basket.host

---
+++ Basket.Host/Controllers/BasketBffController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Basket.Host.Controllers
+{
+    [ApiController]
+    [Authorize(Policy = AuthPolicy.AllowEndUserPolicy)]
+    [Route(ComponentDefaults.DefaultRoute)]
+    public class BasketBffController : ControllerBase
+    {
+    }
+}
---
+++ Basket.Host/Controllers/BasketItemController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Basket.Host.Controllers;
+
+[ApiController]
+[Authorize(Policy = AuthPolicy.AllowClientPolicy)]
+[Scope("basket.basketitem")]
+[Route(ComponentDefaults.DefaultRoute)]
+
+    public class BasketItemController : ControllerBase
+{
+
+    }
+
